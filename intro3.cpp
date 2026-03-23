e99f4cbf41d7c0db0e0b230479ebc99ee841e968
Himanshu Thakur <himanthakur123@gmail.com>
2026-03-23 18:05:21 +0530

    NON BLocking socket

--- intro3.cpp
+++ intro3.cpp
@@ -1,5 +1,14 @@
 //MAIN THINS
+//
+//
+//
+//                      FD_ZERO → FD_SET → select → FD_ISSET
+//
+//
+//
+//
 
+#include <fcntl.h>
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -12,12 +21,16 @@ int main() {
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(9900);
-    addr.sin_addr.s_addr = INADDR_ANY;
 
+    addr.sin_addr.s_addr = INADDR_ANY;
     bind(s, (sockaddr*)&addr, sizeof(addr));
     listen(s, 5);
 
+    fcntl(s, F_SETFL, O_NONBLOCK); //Non-Blocking
+
     while (1) {
+
+
         fd_set f;
         FD_ZERO(&f);        // RESET
         FD_SET(s, &f);      // ADD
@@ -25,7 +38,20 @@ int main() {
         select(s + 1, &f, NULL, NULL, NULL);  // WAIT
 
         if (FD_ISSET(s, &f)) {                // CHECK
-            cout << "Client trying to connect\n";
-        }
+            int client = accept(s, NULL, NULL); //Non-Blocking
+            if (client < 0) {
+
+                // // 🔥 THIS IS THE KEY PART
+                // if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                //     cout << "No client yet (non-blocking)\n";
+                // } else {
+                //     perror("accept error");
+                // }
+
+            } else {
+                cout << "Client connected! FD: " << client << endl;
+            }
     }
+}
+return 0;
 }
